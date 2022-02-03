a188b3e683743a7696ffa51f32c8d9b8408e362a
dineshsharma02 <62809636+dineshsharma02@users.noreply.github.com>
2022-02-03 15:52:30 +0530

    Post like backend created and tested with admin

---
+++ posts/models.py
@@ -0,0 +1,42 @@
+
+from email.policy import default
+from enum import unique
+from django.db import models
+from django.contrib.auth.models import User
+from datetime import datetime
+
+# class InstaUser(models.Model):
+#     # newname = User.first_name+" "User.last_name
+#     username = models.ForeignKey(User, on_delete=models.CASCADE)
+#     name = models.CharField(max_length=60,default=User.first_name)
+    
+#     email = models.EmailField(max_length=254,default=User.email)
+
+
+# Create your models here.
+class Posts(models.Model):
+
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='media/profpics/',default=None)
+    total_likes = models.IntegerField(default=0)
+    total_comments = models.IntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class Post_likes(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE,related_name='user')
+    post_id = models.ForeignKey("posts.Posts", on_delete=models.CASCADE,related_name='post_id',default=None)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        unique_together = ('user_id','post_id')
+
+    
+
+
+# class Tags(models.Model):
+#     tagged = models.ForeignKey(User,default=None)
+
+
+# class Post_Comments(models.Model):
+#     comment_to = models.ForeignKey(User,default=Posts.user)
+#     comment_by = models.ForeignKey
