787668855d966925afd97d137fae59b04b763c18
dineshsharma02 <62809636+dineshsharma02@users.noreply.github.com>
2022-02-06 03:53:34 +0530

    Fixed saving user with post

--- posts/serializer.py
+++ posts/serializer.py
@@ -4,14 +4,22 @@ from rest_framework import serializers
 from .models import Posts
 
 class PostSerializerImageField(serializers.ModelSerializer):
+    def get_user_id(self,obj):
+        self.context.get('user_id')
     # user_id = serializers.PrimaryKeyRelatedField(read_only=True)
     # user_id = serializers.ReadOnlyField()
+    
     image = serializers.ImageField(required=True)
     total_likes = serializers.ReadOnlyField()
     total_comments = serializers.ReadOnlyField()
     class Meta:
         model = Posts
-        fields = '__all__'
-        
+        fields = "__all__"
+    
+    # def create(self, validated_data):
+    #     user_id = self.get_user_id(self,validated_data)
+    #     return Posts(user_id,**validated_data)
+
+    
 
         # edi
--- posts/views.py
+++ posts/views.py
@@ -1,19 +1,32 @@
 
+from attr import validate
 from django.shortcuts import render
 from .serializer import PostSerializerImageField
 from rest_framework import viewsets
 from .models import Posts
+# from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.generics import ListCreateAPIView,ListAPIView,CreateAPIView
 from rest_framework.permissions import IsAuthenticated
 from posts import serializer
+from rest_framework import status
 
 class PostView(ListCreateAPIView):
     queryset = Posts.objects.all()
     serializer_class = PostSerializerImageField
     permission_classes = [IsAuthenticated]
-    
+
+    def create(self, request, *args, **kwargs):
+        data = request.data
         
+        data['user_id'] = request.user.id
+        print(data)
+        serializer = PostSerializerImageField(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'msg':'data created'},status= status.HTTP_201_CREATED)
+        return Response(serializer.errors,status= status.HTTP_400_BAD_REQUEST)
+
         
 
 # class PostView(ListAPIView):
