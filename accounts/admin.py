22993114ecca9fe115e3c32619512be53cbf62cb
dineshsharma02 <62809636+dineshsharma02@users.noreply.github.com>
2022-02-09 14:10:06 +0530

    User follower table created

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,6 +1,10 @@
 from csv import list_dialects
 from django.contrib import admin
 from rest_framework.authtoken.models import Token
+from .models import UserFollower
 
-# 
+@admin.register(UserFollower)
+
+class UserFollowerAdmin(admin.ModelAdmin):
+    list_display = ['user_id','follower_id','created_at']
 
--- accounts/models.py
+++ accounts/models.py
@@ -4,5 +4,12 @@ from django.conf import settings
 from django.db import models
 from django.dispatch import receiver
 from rest_framework.authtoken.models import Token
+from django.contrib.auth.models import User
 
-        
+class UserFollower(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE,related_name='user_id')
+    follower_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('user_id','follower_id')
